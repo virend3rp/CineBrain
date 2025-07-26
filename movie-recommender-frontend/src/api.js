d0769219c95088b391b615f057d1d6c1cc1ee032
Virender Parasariya <parasariya.1@iitj.ac.in>
2025-07-26 21:15:53 +0530

    3rd  commit

--- movie-recommender-frontend/src/api.js
+++ movie-recommender-frontend/src/api.js
@@ -1,6 +1,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:10000";
 
-export async function fetchByTitle(title) {
+export async function API(title) {
   const res = await fetch(`${BASE_URL}/recommend/title?title=${encodeURIComponent(title)}`);
   return res.json();
 }
