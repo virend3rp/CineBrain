150f1db3ac338dbeffc9785273c273cab0b0543c
Virender Parasariya <parasariya.1@iitj.ac.in>
2025-07-27 08:11:18 +0530

    7th  commit

--- movie-recommender-frontend/src/api.js
+++ movie-recommender-frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:10000";
+
 export default async function fetchByTitle(title) {
-  const res = axios.get(`/recommend/title/${movieTitle}`)
-  return res.json();
+  const res = await axios.get(`${BASE_URL}/recommend/title/${title}`);
+  return res.data;
 }
